55522aafee15f093209ad2b1ff3ece171f015cce
Jamil Kabir <jamilkabir719@gmail.com>
2025-02-27 15:28:45 -0500

    Simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/nova-pharmacy/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
   build: {
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'img';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-      },
-    },
-  },
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  }
 })
