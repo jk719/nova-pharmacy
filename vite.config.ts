a8240ee4060d4325422ace133a5d0dbf3a41674d
Jamil Kabir <jamilkabir719@gmail.com>
2025-02-27 19:21:48 -0500

    chore: update dependencies and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nova-pharmacy/',
   plugins: [react()],
   build: {
     assetsDir: 'assets',
